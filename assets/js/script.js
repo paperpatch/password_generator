67617983b8ad405ce00d62d2cb70b4844415236f
paperpatch <patchen21@gmail.com>
2021-10-07 10:58:14 -0400

    condition const to let

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,38 +20,37 @@ var generatePassword = function() {
       generatePassword();
     };
   
-
   let promptLowercase = window.confirm("Do you want to have lowercase in your password?")
-    if (promptLowercase) {
-      promptLowercase = true;
-    } else {
-      promptLowercase = false;
-    };
-  
+  if (promptLowercase) {
+    promptLowercase = true;
+  } else {
+    promptLowercase = false;
+  };
+
   let promptUppercase = window.confirm("Do you want to have uppercases in your password?")
-    if (promptUppercase) {
-      promptUppercase = true;
-    } else {
-      promptUppercase = false;
-    };
-  
-    let promptNumbers = window.confirm("Do you want to have numbers in your password?")
-    if (promptNumbers) {
-      promptNumbers = true;
-    } else {
-      promptNumbers = false;
-    };
-  
-    let promptSymbol = window.confirm("Do you want to have special characters in your password?")
-    if (promptSymbol) {
-      promptSymbol = true;
-    } else {
-      promptSymbol = false;
-    };
+  if (promptUppercase) {
+    promptUppercase = true;
+  } else {
+    promptUppercase = false;
+  };
+
+  let promptNumbers = window.confirm("Do you want to have numbers in your password?")
+  if (promptNumbers) {
+    promptNumbers = true;
+  } else {
+    promptNumbers = false;
+  };
+
+  let promptSymbol = window.confirm("Do you want to have special characters in your password?")
+  if (promptSymbol) {
+    promptSymbol = true;
+  } else {
+    promptSymbol = false;
+  };
 
   // Count how many conditions there are. Check if user input a condition type.
 
-  const conditionsCount = promptLowercase + promptUppercase + promptNumbers + promptSymbol;
+  let conditionsCount = promptLowercase + promptUppercase + promptNumbers + promptSymbol;
 
   if (conditionsCount === 0) {
     window.alert("You need to have at least one character type. Please try again.");
